94ee29dd7f91e2cf75508d56caeb3b715dde5102
JoseILizarraga <99209790+JoseILizarraga@users.noreply.github.com>
2022-02-15 21:33:57 -0700

    Add files via upload

---
+++ primeros-pasos.py
@@ -0,0 +1,35 @@
+# sum = 1 + 2
+# print(sum)
+
+# sum2 = 1 + 2
+# product = sum2 * 2
+# print(product)
+
+# planetas_en_el_sistema_solar = 8
+# distancia_a_alfa_centauri = 4.367
+# puede_despegar = True
+# transbordador_que_aterrizo_en_la_luna = "Apollo 11"
+# print(type(distancia_a_alfa_centauri))
+
+# left_side = 10
+# right_side = 5
+# print(left_side / right_side)
+
+# from datetime import date
+# print(date.today())
+# print("Today's date is: " , date.today())
+# print("Today's date is: " , str(date.today()))
+
+# print("Bienvenido al programa de bienvenida")
+# name = input("Introduzca su nombre ")
+# print("Saludos: " + name)
+
+# print("Calculadora")
+# first_number = input("Primer número: ")
+# second_number = input("Segundo número: ")
+# print(first_number + second_number)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(int(first_number) + int(second_number))
